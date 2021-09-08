48585f3f2e0ccd8bb2019245bffb727a800bf6f4
Fabricio Chiquio Boppré <fabricio.boppre@gmail.com>
2021-09-08 12:50:10 +0000

    Added a label to SearchForm input.

--- components/SearchForm.js
+++ components/SearchForm.js
@@ -6,7 +6,12 @@ export default function SearchForm(props) {
   return (
 		<div id={styles.search_form}>
 			<form>
-				<input type="search" placeholder="título ou autor" value={props.formSearchString} onChange={props.setFormSearchString}/>
+			  <label hidden htmlFor="formSearchString">pesquisar:</label>
+				<input type="search" 
+							 id="formSearchString" 
+							 placeholder="título ou autor" 
+							 value={props.formSearchString} 
+							 onChange={props.setFormSearchString}/>
     		<Link href={`/?search=${encodeURIComponent(props.formSearchString)}`} passHref>
 					<button onClick={() => props.handleSearchButton()}>pesquisar</button>
 				</Link>
