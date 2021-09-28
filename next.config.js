e381e863a284054d8fd26792706dda303db16ad9
Fabricio Chiquio Boppré <fabricio.boppre@gmail.com>
2021-09-28 21:44:10 +0000

    Created the temporary “welcome” page (inside /public).

--- next.config.js
+++ next.config.js
@@ -1,5 +1,14 @@
 // next.config.js
 module.exports = {
+  async redirects() {
+    return [
+      {
+        source: '/boasvindas',
+        destination: '/boasvindas.html',
+        permanent: true,
+      },
+    ]
+  },
   images: {
     domains: ['cdn.sanity.io'],
   },
