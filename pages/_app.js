aeabb13396dd9362260c09d6e8d44a3d2f9481c9
Fabricio Chiquio Boppré <fabricio.boppre@gmail.com>
2022-04-26 08:05:45 -0300

    Changed branch on Algolia configuration.

--- pages/_app.js
+++ pages/_app.js
@@ -93,11 +93,10 @@ export default class Humana extends App {
 							appId: '519SKT1U6K',
 							apiKey: 'd2573d3df1e2b3589328f740fbce7a02',
 							siteId: '63123b87-3b3b-4993-ac15-6b4b33392e6b',
-							branch: 'algolia',
+							branch: 'main',
 							selector: 'div#searchAlgolia',
 							placeholder: 'pesquisar',
 							detached: false,
-							debug: true,
 							hitsPerPage: 20,
 						})	
 					}}
